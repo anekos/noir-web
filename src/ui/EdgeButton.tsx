4c4df0e29f4c8cf46d16ad52187379cf5d698f14
anekos <anekos@snca.net>
2021-09-13 21:41:21 +0900

    Hide edge buttons

--- src/ui/EdgeButton.tsx
+++ src/ui/EdgeButton.tsx
@@ -6,9 +6,10 @@ import classNames from 'classnames'
 export default function EdgeButton({extraClass, onClick, visible}) {
   if (!visible)
     return (<></>)
+  // <button class="transition duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ...">
   return (
     <div
-      className={classNames('absolute bg-green-300 z-50 rounded-md opacity-0 hover:opacity-80', extraClass)}
+      className={classNames('absolute bg-green-300 z-50 rounded-md edge-button', extraClass)}
       onClick={onClick}
     />
   )
