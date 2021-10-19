7ca59523927c63b4451b1ff99f2cc86f96a63e6b
anekos <anekos@snca.net>
2021-10-19 21:00:31 +0900

    Show a scroll bar

--- src/ui/PanelFrame.tsx
+++ src/ui/PanelFrame.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function PanelFrame({children}) {
   return (
-    <div className="z-40 bg-blue-500 p-2 opacity-90 rounded-md flex flex-col items-center">
+    <div className="z-40 bg-blue-500 p-2 opacity-90 rounded-md flex flex-col items-center overflow-y-auto max-h-screen">
       <div className="flex flex-row items-center w-full p-2">
         { children }
       </div>
