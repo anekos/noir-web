9473ca5b1b5325383cb3af204f159ffb2a7f6165
anekos <anekos@snca.net>
2021-10-19 21:00:04 +0900

    Update the clock

--- src/ui/Clock.tsx
+++ src/ui/Clock.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import strftime from 'strftime'
 
 import Information from './Information'
+import useInterval from '../use-interval'
+
+
+function currentTime(): string {
+  return strftime('%Y-%m-%d (%a) %H:%M')
+}
 
 
 export default function Clock() {
+  const [time, setTime] = useState<string>(currentTime())
+
+  useInterval(
+    1,
+    () => setTime(currentTime()),
+  )
+
   return (
     <Information className="absolute right-0 bottom-0 ">
-      { strftime('%Y-%m-%d (%a) %H:%M') }
+      { time }
     </Information>
   )
 }
