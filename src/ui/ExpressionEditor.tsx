9621452d8de416d22cb37e349e233df777a14c32
anekos <anekos@snca.net>
2021-10-27 11:12:05 +0900

    Change the component for suggestion

--- src/ui/ExpressionEditor.tsx
+++ src/ui/ExpressionEditor.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 
 import classNames from 'classnames'
 
-import TextInput from 'react-autocomplete-input'
+import Autosuggest from 'react-autosuggest'
+
 import { SearchHistory } from '../api'
 import { getAliases, getHistory, getTags } from '../api'
 import { useEffectIfMounted } from '../hook/use-effect-if-mounted'
@@ -22,6 +23,12 @@ function sortIgnoreCase(lst: string[]): string[] {
   return Array.from(lst).sort(cmp)
 }
 
+interface Candidate {
+  name: string
+  value: string
+  display: string
+}
+
 interface IExpressionEditor {
   expression: string
   setExpression: (e: string) => void
@@ -32,18 +39,11 @@ export default function ExpressionEditor({expression, setExpression, onSubmit, c
   const [aliases, setAliases] = useState<string[]>([])
   const [history, setHistory] = useState<SearchHistory[]>([])
   const [tags, setTags] = useState<string[]>([])
+  const [suggestions, setSuggestions] = useState<Candidate[]>([])
 
   useEffectIfMounted((ifok) => { getAliases().then(sortIgnoreCase).then(ifok(setAliases)) }, [])
   useEffectIfMounted((ifok) => { getTags().then(sortIgnoreCase).then(ifok(setTags)) }, [])
   useEffectIfMounted((ifok) => { getHistory().then(ifok(setHistory)) }, [])
-
-  const expressions = history.map(it => it.expression)
-
-  function changeOnSelect(trigger, suffix) {
-    if (trigger === '@' || trigger === '!')
-      return suffix
-    return trigger + suffix
-  }
 
   function onKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
     if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) {
@@ -52,16 +52,97 @@ export default function ExpressionEditor({expression, setExpression, onSubmit, c
     }
   }
 
+  const onChange = (event, {newValue, method}) => {
+    setExpression(newValue)
+  }
+
+  const inputProps = {
+    onChange,
+    onKeyPress,
+    placeholder: 'Type a SQL where part',
+    value: expression,
+  };
+
+  const onSuggestionsFetchRequested = ({value})=> {
+    const newSuggestions: Candidate[] = []
+    const maxCandidates = 20
+    const adding = new Set()
+
+    function append<T>(items: T[], name: string, prefix: string, keyCtor: (T) => string, valueCtor: (T) => string) {
+      if (value[0] !== prefix)
+        return
+      let n = 0
+      items.forEach((item: T) => {
+        if (maxCandidates <= n)
+          return
+        const k = keyCtor(item)
+        if (k.indexOf(value.slice(prefix.length)) < 0)
+          return
+        const v = valueCtor(item)
+        if (adding.has(v))
+          return
+        adding.add(v)
+        newSuggestions.push({value: v, name, display: v})
+        n++
+      })
+    }
+
+    append(tags, 'tag', '#', (it) => it, (it) => `#${it}`)
+    append(aliases, 'alias', '@', (it) => it, (it) => it)
+    append(history, 'history', '!', (it) => it.expression, (it) => it.expression)
+
+    setSuggestions(newSuggestions)
+  }
+
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([])
+  }
+
   return (
-    <TextInput
+    <Autosuggest
+      suggestions={suggestions}
+      inputProps={inputProps}
+      {...{
+        getSuggestionValue,
+        onChange,
+        onSuggestionsClearRequested,
+        onSuggestionsFetchRequested,
+        renderInputComponent,
+        renderSuggestion,
+        renderSuggestionsContainer,
+      }}
+    />
+  )
+}
+
+function renderSuggestion(item: Candidate, {query, isHighlighted}) {
+  return (
+    <span className={classNames(isHighlighted && 'font-bold')}>
+      {item.display}
+    </span>
+  )
+}
+
+function getSuggestionValue(suggestion: Candidate) {
+  return suggestion.value
+}
+
+function renderSuggestionsContainer({ containerProps, children, query }) {
+  if (!children)
+    return (<></>)
+  return (
+    <div {...containerProps} className="text-white absolute bg-green-900 drop-shadow-md p-2 rounded-sm z-50">
+      {children}
+    </div>
+  );
+}
+
+function renderInputComponent(inputProps) {
+  return (
+    <textarea
+      {...inputProps}
       autoFocus={true}
-      options={{'@': aliases, '#': tags, '!': expressions}}
-      trigger={['@', '#', '!']}
-      changeOnSelect={changeOnSelect}
-      onChange={setExpression}
-      value={expression}
-      maxOptions={20}
-      onKeyPress={onKeyPress}
-      className={classNames("rounded-md font-bold p-2 w-full", className)} />
+      className={classNames("rounded-md font-bold p-2 w-full text-sm h-24", inputProps.className)}
+    />
   )
 }
